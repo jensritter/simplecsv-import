1684d7633c8411190f0bb1f1476186ccea95adf1
Jens Ritter <jens.ritter@gmail.com>
2024-08-24 01:28:05 +0200

    some jpackage plays

--- create-exe.cmd
+++ create-exe.cmd
@@ -3,11 +3,13 @@ mkdir target
 mkdir target\winbuild
 copy cli\target\simplecsv-cli-1.0-SNAPSHOT-exec.jar target\winbuild
 
-mkdir target\exe
 jpackage --input target\winbuild ^
     --win-console ^
     --name simplecsv ^
     --main-jar simplecsv-cli-1.0-SNAPSHOT-exec.jar ^
     --app-version 1.0 ^
     --type app-image ^
-    -d target\exe
+    --add-modules java.naming,java.desktop,java.management,java.sql ^
+    -d target\
+
+echo see target\simplecsv
